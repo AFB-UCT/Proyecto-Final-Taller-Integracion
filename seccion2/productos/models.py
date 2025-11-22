f3914ef0b259dcf036bccb1047d07f5de93ffad9
AFB-UCT <agustin.flores2024@alu.uct.cl>
2025-11-21 21:37:52 -0300

    modified:   seccion2/db.sqlite3 	new file:   seccion2/productos/migrations/0002_producto_usuario.py 	modified:   seccion2/productos/models.py 	modified:   seccion2/productos/templates/productos/inicio.html 	modified:   seccion2/productos/templates/productos/navbar.html 	modified:   seccion2/productos/templates/productos/obtener_productos.html 	new file:   seccion2/productos/templates/registration/login.html 	modified:   seccion2/productos/views.py 	modified:   seccion2/seccion2/settings.py 	modified:   seccion2/seccion2/urls.py

--- seccion2/productos/models.py
+++ seccion2/productos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,6 +7,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     cantidad = models.IntegerField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
--- seccion2/productos/views.py
+++ seccion2/productos/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 from .models import Producto
 
 # Create your views here.
@@ -27,11 +29,15 @@ def porId(request, id):
 
 def eliminarId(request, id):
     producto = get_object_or_404(Producto, id=id)
+    if not request.user.is_staff and producto.usuario != request.user:
+        return HttpResponseForbidden("No tienes permiso para eliminar este producto.")
     producto.delete()
     return redirect('lista_productos')
 
 def actualizarId(request, id):
     producto = get_object_or_404(Producto, id=id)
+    if not request.user.is_staff and producto.usuario != request.user:
+        return HttpResponseForbidden("No tienes permiso para editar este producto.")
     if request.method == 'POST':
         producto.nombre = request.POST.get('nombre')
         producto.descripcion = request.POST.get('descripcion')
@@ -42,12 +48,14 @@ def actualizarId(request, id):
         context = {'producto': producto}
         return render(request, 'productos/producto_actualizar.html', context)
 
+@login_required
 def agregarProducto(request):
     if request.method == 'POST':
         nombre = request.POST.get('nombre')
         descripcion = request.POST.get('descripcion')
         cantidad = request.POST.get('cantidad')
         nuevo_producto = Producto(nombre=nombre, descripcion=descripcion, cantidad=cantidad)
+        nuevo_producto.usuario = request.user
         nuevo_producto.save()
         return redirect('lista_productos')
     else:
